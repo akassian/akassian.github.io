e643e89af9b926e88b94427647144f338c26dd31
akass1122 <akassian@berkeley.edu>
2024-08-04 19:36:51 -0700

    try pushing build for GH-pages pipeline from master instead

---
+++ build/precache-manifest.5f1df58d322f2535c7ee25becf3b39c4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1807ba430c42881ebf43b0962b3af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d276cfec8fc3594975cf",
+    "url": "/static/css/main.f1b42223.chunk.css"
+  },
+  {
+    "revision": "8c9822e4c7787dfc336f",
+    "url": "/static/js/2.cfb5410c.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.cfb5410c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d276cfec8fc3594975cf",
+    "url": "/static/js/main.487ce142.chunk.js"
+  },
+  {
+    "revision": "62b93c90eabdcc51201a",
+    "url": "/static/js/runtime-main.4bce2179.js"
+  },
+  {
+    "revision": "2cff70f4d167c009e3f7953727fd827e",
+    "url": "/static/media/axe.2cff70f4.gif"
+  },
+  {
+    "revision": "6ae1984e6cb8872e90a6ad8d8859aed2",
+    "url": "/static/media/axe.6ae1984e.png"
+  },
+  {
+    "revision": "5aecf4fc7477942090128eb8948de243",
+    "url": "/static/media/bearcon.5aecf4fc.gif"
+  },
+  {
+    "revision": "9fd497d0909c483fd870260ca76b5ef0",
+    "url": "/static/media/bearcon.9fd497d0.png"
+  },
+  {
+    "revision": "5b0fbe02c67e7c0bde6d2f54b3d25551",
+    "url": "/static/media/commerce-center.5b0fbe02.png"
+  },
+  {
+    "revision": "f4ec01a74c4fd9cc72f9c37036510c25",
+    "url": "/static/media/headshot.f4ec01a7.png"
+  },
+  {
+    "revision": "88a5e184c21526e263031479044ee8fa",
+    "url": "/static/media/headshot2.88a5e184.png"
+  },
+  {
+    "revision": "e9a7c7d549bd573c068ef8a8347b9763",
+    "url": "/static/media/partner-center.e9a7c7d5.png"
+  },
+  {
+    "revision": "8ecb3114f6c3162e2605f6bd783ed755",
+    "url": "/static/media/partner-center2.8ecb3114.png"
+  },
+  {
+    "revision": "957f11401d94367c9f5028e8cb0e7759",
+    "url": "/static/media/warbler.957f1140.gif"
+  },
+  {
+    "revision": "f9b098f23a81f5f4879783cb52b7c506",
+    "url": "/static/media/warbler.f9b098f2.png"
+  },
+  {
+    "revision": "11ce723173d47873a076aa8cf7a6bd87",
+    "url": "/static/media/waves.11ce7231.png"
+  }
+]);
